14e24d4fc02659688c38105b58e12ce5fa06b1d3
Felix Schmidt <felix.fs.schmidt@bmw.de>
2018-11-17 11:33:51 +0100

    Implement a GerritRepository class based on the generic GitRepository
    As the Gerrit API does not support everthing we need, we could use the
    basic functions of the generic GitRepository implementation and just add
    the Gerrit specific URLs to be shown in zubbi.

---
+++ zubbi/scraper/repos/gerrit.py
@@ -0,0 +1,43 @@
+# Copyright 2018 BMW Car IT GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from zubbi.scraper.repos.git import GitRepository
+
+
+class GerritRepository(GitRepository):
+    # NOTE (fschmidt): As the Gerrit API does not support everything we need,
+    # we let the GitRepository do all the checkouts and directory listings
+    # and use the gerrit API only for building the URLs which are shown in zubbi.
+
+    def __init__(self, repo_name, workspace_dir, remote_url, gerrit_con):
+        super().__init__(repo_name, workspace_dir, remote_url)
+        self.gerrit_con = gerrit_con
+
+    def get_url_for_file(self, file_path, highlight_start=None, highlight_end=None):
+        file_url = "{}?p={}.git;a=blob;f={}".format(
+            self.gerrit_con.gitweb_url, self.repo_name, file_path
+        )
+
+        if highlight_start is not None:
+            file_url = "{}#l{}".format(file_url, highlight_start)
+
+        # NOTE (fschmidt): highlighting a range is not supported by gerrit/gitweb
+
+        return file_url
+
+    def get_url_for_directory(self, directory_path):
+        url = "{}?p={}.git;a=tree;f={}".format(
+            self.gerrit_con.gitweb_url, self.repo_name, directory_path
+        )
+        return url
